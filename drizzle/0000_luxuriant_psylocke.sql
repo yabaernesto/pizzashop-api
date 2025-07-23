be034796bdf6cb148736e05db60aa012c025aa74
Yaba Ernesto <yabaernesto@gmail.com>
2025-07-23 17:01:31 +0100

    initial commit

---
+++ drizzle/0000_luxuriant_psylocke.sql
@@ -0,0 +1,19 @@
+CREATE TYPE "public"."user_role" AS ENUM('manager', 'customer');--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" text PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"email" text NOT NULL,
+	"phone" text,
+	"role" "user_role" DEFAULT 'customer' NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
+CREATE TABLE "restaurants" (
+	"id" text PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"description" text,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
