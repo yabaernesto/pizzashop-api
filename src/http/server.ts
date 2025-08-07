0f087ad9a53ac480c7ce3fd879915fd6a3e1bf83
Yaba Ernesto <yabaernesto@gmail.com>
2025-08-07 17:11:07 +0100

    feat: rota que trás dados do usuário logado

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,7 +2,8 @@ import { Elysia } from 'elysia'
 
 import { env } from '../env'
 
-import { authenticateFormLink } from './routes/authenticate-form-link'
+import { authenticateFromLink } from './routes/authenticate-from-link'
+import { getProfile } from './routes/get-profile'
 import { registerRestaurant } from './routes/register-restaurant'
 import { sendAuthLink } from './routes/send-auth-link'
 import { signOut } from './routes/sign-out'
@@ -10,8 +11,9 @@ import { signOut } from './routes/sign-out'
 const app = new Elysia()
   .use(registerRestaurant)
   .use(sendAuthLink)
-  .use(authenticateFormLink)
+  .use(authenticateFromLink)
   .use(signOut)
+  .use(getProfile)
 
 app.listen(env.PORT, () => {
   // biome-ignore lint/suspicious/noConsole: show server
