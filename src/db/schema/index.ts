d38f142db7ee619e5d93332d8c94def51bdb33f8
Yaba Ernesto <yabaernesto@gmail.com>
2025-08-09 22:12:00 +0100

    feat: schema de produtos e pedidos

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,13 +1,22 @@
-import * as authLinks from './auth-links'
-import * as restaurants from './restaurants'
-import * as users from './users'
+// import * as authLinks from './auth-links'
+// import * as orderItems from './order-items'
+// import * as orders from './orders'
+// import * as products from './products'
+// import * as restaurants from './restaurants'
+// import * as users from './users'
 
 export * from './auth-links'
+export * from './order-items'
+export * from './orders'
+export * from './products'
 export * from './restaurants'
 export * from './users'
 
-export const schema = {
-  ...users,
-  ...restaurants,
-  ...authLinks,
-}
+// export const schema = {
+//   ...users,
+//   ...restaurants,
+//   ...authLinks,
+//   ...orders,
+//   ...products,
+//   ...orderItems,
+// }
