a203cc874c29e9148f0d4341ac90577b951d078c
Yaba Ernesto <yabaernesto@gmail.com>
2025-08-12 21:51:50 +0100

    feat: rota de receita mensal

---
+++ src/http/routes/get-month-receipt.ts
@@ -0,0 +1,64 @@
+import dayjs from 'dayjs'
+import { and, eq, gte, sql, sum } from 'drizzle-orm'
+import Elysia from 'elysia'
+
+import { db } from '../../db/connection'
+import { orders } from '../../db/schema'
+import { auth } from '../auth'
+import { UnauthorizedError } from '../errors/unauthorized-error'
+
+export const getMonthReceipt = new Elysia()
+  .use(auth)
+  .get('/metrics/month-receipt', async ({ getCurrentUser }) => {
+    const { restaurantId } = await getCurrentUser()
+
+    if (!restaurantId) {
+      throw new UnauthorizedError()
+    }
+
+    const today = dayjs()
+    const lastMonth = today.subtract(1, 'month')
+    const startOfLastMonth = lastMonth.startOf('month')
+
+    // Busca os recebimentos agrupados por ano-mês
+    const monthsReceipts = await db
+      .select({
+        monthWithYear: sql<string>`TO_CHAR(${orders.createdAt}, 'YYYY-MM')`,
+        receipt: sum(orders.totalInCents).mapWith(Number),
+      })
+      .from(orders)
+      .where(
+        and(
+          eq(orders.restaurantId, restaurantId),
+          gte(orders.createdAt, startOfLastMonth) // pega desde o início do mês passado
+        )
+      )
+      .groupBy(sql`TO_CHAR(${orders.createdAt}, 'YYYY-MM')`)
+
+    const currentMonthWithYear = today.format('YYYY-MM')
+    const lastMonthWithYear = lastMonth.format('YYYY-MM')
+
+    const currentMonthReceipt = monthsReceipts.find(
+      (monthReceipt) => monthReceipt.monthWithYear === currentMonthWithYear
+    )
+
+    const lastMonthReceipt = monthsReceipts.find(
+      (monthReceipt) => monthReceipt.monthWithYear === lastMonthWithYear
+    )
+
+    let diffFromLastMonth = 0
+    if (
+      currentMonthReceipt &&
+      lastMonthReceipt &&
+      lastMonthReceipt.receipt > 0
+    ) {
+      const percentChange =
+        (currentMonthReceipt.receipt * 100) / lastMonthReceipt.receipt
+      diffFromLastMonth = Number((percentChange - 100).toFixed(2))
+    }
+
+    return {
+      receipt: currentMonthReceipt?.receipt ?? 0,
+      diffFromLastMonth,
+    }
+  })
--- src/http/server.ts
+++ src/http/server.ts
@@ -8,6 +8,7 @@ import { cancelOrder } from './routes/cancel-order'
 import { deliverOrder } from './routes/deliver-order'
 import { dispatchOrder } from './routes/dispatch-order'
 import { getManagedRestaurant } from './routes/get-managed-restaurant'
+import { getMonthReceipt } from './routes/get-month-receipt'
 import { getOrderDetails } from './routes/get-order-details'
 import { getOrders } from './routes/get-orders'
 import { getProfile } from './routes/get-profile'
@@ -29,6 +30,7 @@ const app = new Elysia()
   .use(deliverOrder)
   .use(dispatchOrder)
   .use(getOrders)
+  .use(getMonthReceipt)
   .onError(({ code, error, set }) => {
     switch (code) {
       case 'VALIDATION': {
