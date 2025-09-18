097d737227e74b898c63bf1312d5fe064e13b2f2
Yaba Ernesto <yabaernesto@gmail.com>
2025-09-18 17:28:13 +0100

    feat(orders): adiciona rotas para aprovação, entrega, despacho e relatórios

---
+++ src/http/routes/approve-order.ts
@@ -0,0 +1,50 @@
+import { eq } from 'drizzle-orm'
+import { type Elysia, t } from 'elysia'
+
+import { db } from '../../db/connection'
+import { orders } from '../../db/schema'
+import { auth } from '../auth'
+import { UnauthorizedError } from '../errors/unauthorized-error'
+
+export const approveOrder = (app: Elysia) => {
+  return app.use(auth).patch(
+    '/orders/:orderId/approve',
+    async ({ getCurrentUser, set, params }) => {
+      const { orderId } = params
+      const { restaurantId } = await getCurrentUser()
+
+      if (!restaurantId) {
+        throw new UnauthorizedError()
+      }
+
+      const order = await db.query.orders.findFirst({
+        // biome-ignore lint/nursery/noShadow: imports
+        where(fields, { eq }) {
+          return eq(fields.id, orderId)
+        },
+      })
+
+      if (!order) {
+        set.status = 400
+
+        return { message: 'Order not found.' }
+      }
+
+      if (order.status !== 'pending') {
+        set.status = 400
+
+        return { message: 'You can only approve pending orders.' }
+      }
+
+      await db
+        .update(orders)
+        .set({ status: 'processing' })
+        .where(eq(orders.id, orderId))
+    },
+    {
+      params: t.Object({
+        orderId: t.String(),
+      }),
+    }
+  )
+}
